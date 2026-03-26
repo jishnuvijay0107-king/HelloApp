27ec1a9f559503e5b3da9951b4f109426ca29a61
jishnuvijay0107-king <jishnuvijay0107@gmail.com>
2026-03-26 09:35:35 +0530

    UC4: Support multiple names with default World

--- HelloApp/src/main/java/org/example/HelloApp.java
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -1,7 +1,12 @@
 package org.example;
 public class HelloApp {
     public static void main(String[] args) {
-        String name = (args.length > 0) ? args[0] : "World";
-        System.out.println("Hello " + name);
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        }
     }
 }
