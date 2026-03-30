5e07b147d76b2c283cbbf9ec29e57599802872e3
jishnuvijay0107-king <jishnuvijay0107@gmail.com>
2026-03-30 10:28:50 +0530

    UC5: Support multiple names with default World

--- HelloApp/src/main/java/org/example/HelloApp.java
+++ HelloApp/src/main/java/org/example/HelloApp.java
@@ -1,12 +1,17 @@
 package org.example;
+
 public class HelloApp {
     public static void main(String[] args) {
+
+        // Check if no arguments are provided
         if (args.length == 0) {
-            System.out.println("Hello World");
-        } else {
-            for (String name : args) {
-                System.out.println("Hello " + name);
-            }
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        // Enhanced for loop to process multiple names
+        for (String name : args) {
+            System.out.println("Hello, " + name + "!");
         }
     }
 }
